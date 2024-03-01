29fcca8b3ba6dea496e0d551de7821843fd8d374
Carlo Dandan <carlodandan.personal@proton.me>
2024-03-01 16:28:32 +0000

    messi-prebuilt: Synced with v0002 build. * https://github.com/carlodandan/android_kernel_realme_sm6225/releases/tag/v0002
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

---
+++ kernel-headers/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Fri Mar 1 15:06:34 UTC 2024"
+#define LINUX_COMPILE_BY "build-user"
+#define LINUX_COMPILE_HOST "build-host"
+#define LINUX_COMPILER "Android (6875598, based on r399163b) clang version 11.0.5 (https://android.googlesource.com/toolchain/llvm-project 87f1315dfbea7c137aa2e6d362dbb457e388158d), LLD 11.0.5 (/buildbot/tmp/tmpx1DlI_ 87f1315dfbea7c137aa2e6d362dbb457e388158d)"
